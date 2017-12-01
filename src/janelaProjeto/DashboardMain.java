466b1930fa3f4ddd25363b5a52ce52752eaa040f
Mateus G <Mateus G@MateusG>
2017-12-01 21:27:05 -0200

    Adicionando pacote de janela tarefa e adicionando classe de janela tarefa

--- src/janelaProjeto/DashboardMain.java
+++ src/janelaProjeto/DashboardMain.java
@@ -1,19 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package dashboard;
+package janelaProjeto;
 
-/**
- *
- * @author cassio.reis
- */
 public class DashboardMain extends javax.swing.JFrame {
 
-    /**
-     * Creates new form DashboardMain
-     */
     public DashboardMain() {
         initComponents();
     }
---
+++ src/janelaTarefa/janelaTarefa.java
@@ -0,0 +1,5 @@
+package janelaTarefa;
+
+public class janelaTarefa {
+    
+}
