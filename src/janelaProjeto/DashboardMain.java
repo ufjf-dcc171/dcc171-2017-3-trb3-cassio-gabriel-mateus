16dd895f6194800d3a1b6e4f3ca0e8998c9901c5
Mateus G <Mateus G@MateusG>
2017-12-02 19:23:07 -0200

    Adicionando Dashboard

--- src/janelaProjeto/DashboardMain.java
+++ src/janelaProjeto/DashboardMain.java
@@ -23,35 +23,28 @@ public class DashboardMain extends javax.swing.JFrame {
     private void initComponents() {
 
         jlProjetos = new javax.swing.JLabel();
-        jlTarefas = new javax.swing.JLabel();
-        btnDetails = new javax.swing.JButton();
-        btnNewProject = new javax.swing.JButton();
-        btnNewTask = new javax.swing.JButton();
-        btAddPeople = new javax.swing.JButton();
+        jTextField1 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList<>();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jlProjetos.setText("Projetos:");
+        jlProjetos.setText("Projeto:");
 
-        jlTarefas.setText("Tarefas:");
-
-        btnDetails.setText("Ver Detalhes");
-
-        btnNewProject.setText("Novo Projeto");
-        btnNewProject.addActionListener(new java.awt.event.ActionListener() {
+        jTextField1.setText("Digite aqui o nome do novo projeto...");
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnNewProjectActionPerformed(evt);
+                jTextField1ActionPerformed(evt);
             }
         });
 
-        btnNewTask.setText("Nova Tarefa");
-        btnNewTask.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnNewTaskActionPerformed(evt);
-            }
-        });
+        jButton1.setText("Criar Projeto");
 
-        btAddPeople.setText("Adicionar Pessoas");
+        jScrollPane1.setViewportView(jList1);
+
+        jButton2.setText("Ver detalhes");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -59,55 +52,47 @@ public class DashboardMain extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane1)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jlProjetos)
-                        .addGap(198, 198, 198)
-                        .addComponent(jlTarefas))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(btnNewProject)
-                        .addGap(68, 68, 68)
-                        .addComponent(btnNewTask)
-                        .addGap(96, 96, 96)
-                        .addComponent(btnDetails)
-                        .addGap(81, 81, 81)
-                        .addComponent(btAddPeople)))
-                .addContainerGap(81, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTextField1)))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(29, 29, 29)
+                .addGap(25, 25, 25)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jlProjetos)
-                    .addComponent(jlTarefas))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 356, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnNewProject)
-                    .addComponent(btnNewTask)
-                    .addComponent(btAddPeople)
-                    .addComponent(btnDetails))
-                .addGap(60, 60, 60))
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 263, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton2))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnNewProjectActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNewProjectActionPerformed
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_btnNewProjectActionPerformed
-
-    private void btnNewTaskActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNewTaskActionPerformed
-                // TODO add your handling code here:
-    }//GEN-LAST:event_btnNewTaskActionPerformed
+    }//GEN-LAST:event_jTextField1ActionPerformed
 
  
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btAddPeople;
-    private javax.swing.JButton btnDetails;
-    private javax.swing.JButton btnNewProject;
-    private javax.swing.JButton btnNewTask;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JList<String> jList1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField jTextField1;
     private javax.swing.JLabel jlProjetos;
-    private javax.swing.JLabel jlTarefas;
     // End of variables declaration//GEN-END:variables
 }
--- src/janelaTarefa/janelaTarefa.java
+++
@@ -1,5 +0,0 @@
-package janelaTarefa;
-
-public class janelaTarefa {
-    
-}
