4d7c29b8c001c87c6a40d7efa557a60a8dd3ca29
Cassio Reis <cassio.reis@engenharia.ufjf.br>
2017-12-02 17:52:11 -0200

    Adicionando botões no dashboardmain adicionando Listmodel nos projetos e tarefas

---
+++ src/controlDashBoard/ProjectListModel.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlDashBoard;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.event.ListDataListener;
+
+/**
+ *
+ * @author cassio
+ */
+public class ProjectListModel {
+    private final List<Mesa> mesas;
+    private final List<ListDataListener> dataListeners;
+
+    public MesaListModel(List<Mesa> mesas) {
+        this.mesas = mesas;
+        this.dataListeners = new ArrayList<>();
+    }
+
+    @Override
+    public int getSize() {
+        return mesas.size();
+    }
+
+    @Override
+    public Mesa getElementAt(int index) {
+        return mesas.get(index);
+    }
+
+    @Override
+    public void addListDataListener(ListDataListener l) {
+        this.dataListeners.add(l);
+    }
+
+    @Override
+    public void removeListDataListener(ListDataListener l) {
+        this.dataListeners.remove(l);
+    }
+    
+    
+    
+    
+}
--- src/janelaProjeto/DashboardMain.java
+++ src/janelaProjeto/DashboardMain.java
@@ -1,10 +1,23 @@
 package janelaProjeto;
 
-public class DashboardMain extends javax.swing.JFrame {
+import java.awt.Dimension;
 
+public class DashboardMain extends javax.swing.JFrame {
+    
+    
+    
     public DashboardMain() {
-        initComponents();
+      super("Mesas");
+      lstMesas.setModel(new MesaListModel(mesas));
+      
+      
+      
+      initComponents();
+      this.mesas = sampleData;
+      janelaRel = new JanelaRelatorio(this);
     }
+    
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -15,57 +28,92 @@ public class DashboardMain extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jlProjetos = new javax.swing.JLabel();
+        jlTarefas = new javax.swing.JLabel();
+        btnDetails = new javax.swing.JButton();
+        btnNewProject = new javax.swing.JButton();
+        btnNewTask = new javax.swing.JButton();
+        btAddPeople = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jlProjetos.setText("Projetos:");
+
+        jlTarefas.setText("Tarefas:");
+
+        btnDetails.setText("Ver Detalhes");
+
+        btnNewProject.setText("Novo Projeto");
+        btnNewProject.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnNewProjectActionPerformed(evt);
+            }
+        });
+
+        btnNewTask.setText("Nova Tarefa");
+        btnNewTask.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnNewTaskActionPerformed(evt);
+            }
+        });
+
+        btAddPeople.setText("Adicionar Pessoas");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jlProjetos)
+                        .addGap(198, 198, 198)
+                        .addComponent(jlTarefas))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnNewProject)
+                        .addGap(68, 68, 68)
+                        .addComponent(btnNewTask)
+                        .addGap(96, 96, 96)
+                        .addComponent(btnDetails)
+                        .addGap(81, 81, 81)
+                        .addComponent(btAddPeople)))
+                .addContainerGap(81, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jlProjetos)
+                    .addComponent(jlTarefas))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 356, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnNewProject)
+                    .addComponent(btnNewTask)
+                    .addComponent(btAddPeople)
+                    .addComponent(btnDetails))
+                .addGap(60, 60, 60))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(DashboardMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(DashboardMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(DashboardMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(DashboardMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void btnNewProjectActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNewProjectActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnNewProjectActionPerformed
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new DashboardMain().setVisible(true);
-            }
-        });
-    }
+    private void btnNewTaskActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNewTaskActionPerformed
+                // TODO add your handling code here:
+    }//GEN-LAST:event_btnNewTaskActionPerformed
 
+ 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btAddPeople;
+    private javax.swing.JButton btnDetails;
+    private javax.swing.JButton btnNewProject;
+    private javax.swing.JButton btnNewTask;
+    private javax.swing.JLabel jlProjetos;
+    private javax.swing.JLabel jlTarefas;
     // End of variables declaration//GEN-END:variables
 }
