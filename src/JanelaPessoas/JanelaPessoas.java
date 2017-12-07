176b8b268a98c915fdf50567dafff8c8c4b571a8
Gabriel <Gabriel@Gabriel-PC>
2017-12-07 16:12:52 -0200

    Arrumando a operação alterarPessoa na JanelaPessoas

--- src/JanelaPessoas/JanelaPessoas.java
+++ src/JanelaPessoas/JanelaPessoas.java
@@ -15,6 +15,7 @@ import controlDashBoard.Task;
 import janelaControleProjeto.Dashboard;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -54,31 +55,28 @@ public class JanelaPessoas extends JFrame {
             @Override
             public void actionPerformed(ActionEvent ae) {
                 switch (opcoes.getSelectedIndex()) {
-                    case 0:
-                    {
+                    case 0: {
                         painel.removeAll();
                         painel.updateUI();
                         addPessoa();
                         break;
                     }
-                    case 1:
-                    {
+                    case 1: {
                         painel.removeAll();
                         painel.updateUI();
                         {
                             try {
-                            alterarPessoa();
-                        } catch (Exception ex) {
-                            Logger.getLogger(JanelaPessoas.class.getName()).log(Level.SEVERE, null, ex);
-                        }
+                                alterarPessoa();
+                            } catch (Exception ex) {
+                                Logger.getLogger(JanelaPessoas.class.getName()).log(Level.SEVERE, null, ex);
+                            }
                         }
                         break;
                     }
-                    case 2:
-                    {
+                    case 2: {
                         painel.removeAll();
                         painel.updateUI();
-                       try {
+                        try {
                             removerPessoa();
                         } catch (Exception ex) {
                             Logger.getLogger(JanelaPessoas.class.getName()).log(Level.SEVERE, null, ex);
@@ -105,7 +103,7 @@ public class JanelaPessoas extends JFrame {
         horizontal2.add(email);
         vertical.add(horizontal1);
         vertical.add(horizontal2);
-        vertical.add(btnConfirmar, BorderLayout.WEST);
+        vertical.add(btnConfirmar, BorderLayout.CENTER);
         painel.add(vertical);
 
         btnConfirmar.addActionListener(new ActionListener() {
@@ -130,29 +128,43 @@ public class JanelaPessoas extends JFrame {
 
     private void alterarPessoa() throws Exception {
         JList<Pessoa> listaPessoas = new JList<>(new DefaultListModel<>());
-        List<Pessoa> p = daoPessoa.listarTodos();
+        Box vertical = Box.createVerticalBox();
+        Box horizontal1 = Box.createHorizontalBox();
+        Box horizontal2 = Box.createHorizontalBox();
         JButton btnAlterar = new JButton("Alterar");
+        nome = new JTextField(30);
+        email = new JTextField(30);
+        horizontal1.add(new JLabel("Nome: "));
+        horizontal1.add(nome);
+        horizontal2.add(new JLabel("E-mail: "));
+        horizontal2.add(email);
+        vertical.add(new JScrollPane(listaPessoas));
+        vertical.add(horizontal1);
+        vertical.add(horizontal2);
+        vertical.add(btnAlterar);
+        List<Pessoa> p = daoPessoa.listarTodos();
         listaPessoas.setModel(new PessoasListModel(p));
-        listaPessoas.setMinimumSize(new Dimension(100,400));
-        listaPessoas.setPreferredSize(new Dimension(100,400));
+        listaPessoas.setMinimumSize(new Dimension(100, 400));
+        listaPessoas.setPreferredSize(new Dimension(100, 400));
         listaPessoas.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        painel.add(new JScrollPane(listaPessoas), BorderLayout.CENTER);
-        painel.add(btnAlterar);
+        painel.add(vertical);
         btnAlterar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
                 Pessoa pe = listaPessoas.getSelectedValue();
-                if (pe != null)
-                {
-                    nome = new JTextField(pe.getPesNome());
-                    email = new JTextField(pe.getPesEmail());
-                    painel.add(nome, BorderLayout.SOUTH);
-                    painel.add(email, BorderLayout.SOUTH);
-                    painel.updateUI();
-                }
-                else
-                {
-                    // colocar JOptionPane
+                if (pe != null) {
+                    try {
+                        if ("".equals(email.getText()) || "".equals(nome.getText())) {
+                            JOptionPane.showMessageDialog(null, "Preencher todos os campos.", "Por favor preencha todos os campos.", JOptionPane.INFORMATION_MESSAGE);
+                        } else {
+                            // Faça as alterações aqui <<<<<<<<<<<<<<<<<<<<<<<
+                            JOptionPane.showMessageDialog(null, "Uma pessoa foi alterada.", "Pessoa alterada com sucesso.", JOptionPane.INFORMATION_MESSAGE);
+                        }
+                    } catch (Exception ex) {
+                        Logger.getLogger(Dashboard.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(null, "Você deve selecionar a pessoa que deseja alterar.", "Selecione uma pessoa.", JOptionPane.INFORMATION_MESSAGE);
                 }
             }
         });
@@ -160,25 +172,25 @@ public class JanelaPessoas extends JFrame {
 
     private void removerPessoa() throws Exception {
         JList<Pessoa> listaPessoas = new JList<>(new DefaultListModel<>());
+        Box vertical = Box.createVerticalBox();
         JButton btnRemover = new JButton("Remover");
         TaskDAO daoTask = new TaskDAOJDBC();
         TaskPessoaDAO daoTaskPessoa = new TaskPessoaDAOJDBC();
         ProjetoDAO daoProjeto = new ProjetoDAOJDBC();
         listaPessoas.setModel(new PessoasListModel(pessoas));
-        listaPessoas.setMinimumSize(new Dimension(100,400));
-        listaPessoas.setPreferredSize(new Dimension(100,400));
+        listaPessoas.setMinimumSize(new Dimension(350, 300));
+        listaPessoas.setPreferredSize(new Dimension(350, 300));
         listaPessoas.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        painel.add(new JScrollPane(listaPessoas), BorderLayout.CENTER);
-        painel.add(btnRemover);
-        
+        vertical.add(new JScrollPane(listaPessoas));
+        vertical.add(btnRemover, BorderLayout.CENTER);
+        painel.add(vertical);
+
         btnRemover.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent ae) { 
+            public void actionPerformed(ActionEvent ae) {
                 try {
-                    for (Project p : daoProjeto.listarTodos())
-                    {
-                        for (Task t : daoTask.listarTodos(p.getId()))
-                        {
+                    for (Project p : daoProjeto.listarTodos()) {
+                        for (Task t : daoTask.listarTodos(p.getId())) {
                             daoTaskPessoa.buscar(t, pessoas);
                         }
                         System.out.println(p.getTarefas().size());
